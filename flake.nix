0828c958551c00f1d26b3bf6532932be2a5f4852
Nils Imhoff <st180569@stud.uni-stuttgart.de>
2024-12-01 06:02:04 +0100

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Advent of Code with Zig using Nix Flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells = {
+      x86_64-linux = let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+        };
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          zig   # Zig-Compiler
+          git   # Git für Versionskontrolle
+          gdb   # Debugger
+        ];
+      };
+    };
+  };
+}
